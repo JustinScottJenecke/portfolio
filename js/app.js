c2fc7e2e6c20aa533e0b99ece517831fe22d699c
JustinScottJenecke <75792666+JustinScottJenecke@users.noreply.github.com>
2021-05-22 14:34:27 +0200

    initial commit. added files from most recent portfolio project

---
+++ js/app.js
@@ -0,0 +1,63 @@
+const projectsBox = document.querySelectorAll('.box')
+const projectCarouselIndicators = document.querySelectorAll('.project-indicator')
+
+let activeProjectIndicator = 2
+/*let activeSlideIndicator = 2*/
+
+const projects = Array.from(projectsBox)
+const indicators = Array.from(projectCarouselIndicators)
+console.log(projects)
+
+const nextProject = document.querySelector('.project-box5')
+const previousProject = document.querySelector('.project-box4')
+
+console.log(indicators)
+
+previousProject.addEventListener('click', () => {
+ 
+    
+    activeProjectIndicator--
+    
+    if(activeProjectIndicator != 0){
+
+        projects.forEach(project => {
+            project.classList.remove('activeBox')
+
+        if (project.dataset.tag == activeProjectIndicator)
+            project.classList.add('activeBox')
+        })
+
+        indicators.forEach(indicator => {
+            indicator.classList.remove('activeIndicator')
+
+        if (indicator.dataset.ptag == activeProjectIndicator)
+            indicator.classList.add('activeIndicator')
+        })
+
+    } else
+        activeProjectIndicator++
+})
+
+nextProject.addEventListener('click', () => {
+
+    activeProjectIndicator++
+
+    if(activeProjectIndicator <= projects.length){
+
+        projects.forEach(project => {
+            project.classList.remove('activeBox')
+
+        if(project.dataset.tag == activeProjectIndicator)
+            project.classList.add('activeBox')
+        });
+
+        indicators.forEach(indicator => {
+            indicator.classList.remove('activeIndicator')
+            
+        if (indicator.dataset.ptag == activeProjectIndicator)
+            indicator.classList.add('activeIndicator')
+        })
+
+    } else
+        activeProjectIndicator--
+})
